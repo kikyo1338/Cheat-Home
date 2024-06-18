7260d08741509deb819212dfcd0b5feb49bbe97c
kikyo1338 <198672024@qq.com>
2024-06-18 20:22:59 +0800

    Add files via upload

---
+++ src/components/MoreContent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="more-content">本站软件仅供学习交流，严禁用于商业用途，请于24小时内删除。</div>
+</template>
+
+<style lang="scss" scoped>
+.more-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  width: 100%;
+  height: 100%;
+}
+</style>
